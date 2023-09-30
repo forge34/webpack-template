cdb9c32c43bdfc706f4bdf27fcec75e52f5eed8b
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2023-09-30 14:37:52 +0300

    added config files

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+
+  entry: {
+
+    app: './src/index.js',
+
+  },
+
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+
+    ],
+
+  },
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      title: 'Todo App',
+      template:'./src/html/index.html'
+
+    }),
+
+  ],
+
+  output: {
+
+    filename: '[name].bundle.js',
+
+    path: path.resolve(__dirname, 'dist'),
+
+    clean: true,
+
+  },
+
+};
